7b472921edd56cf6d3dcc5ad69560f338e101f9f
Michael Kuron <michael@purple.physcip.uni-stuttgart.de>
2013-04-13 12:36:41 +0200

    Added some logging to the inithomedir script

--- inithomedir.sh
+++ inithomedir.sh
@@ -18,7 +18,7 @@ for arg in $SSH_ORIGINAL_COMMAND; do
 done
 
 if [ "$username" = "" ]; then
-	echo "No username specified"
+	echo "No username specified" | tee -a /var/log/physreg-home.log
 	exit
 fi
 
@@ -51,21 +51,23 @@ if [ ! -d "/System/Library/User Template/$lang.lproj" ]; then
 	esac	
 fi
 
-echo "Creating homedir for $username with $lang"
+echo "Creating homedir for $username with $lang" | tee -a /var/log/physreg-home.log
 
 #createhomedir -c -u $username
 
 groupname=$(id -g -n $username)
 
 cd /Volumes/home
-sudo mkdir $username
-sudo chown $username:$groupname $username
+mkdir $username 2>&1 | tee -a /var/log/physreg-home.log
+chown $username:$groupname $username 2>&1 | tee -a /var/log/physreg-home.log
 
 if [ ! -d "$username/Library/Preferences" ]; then
-	echo "Initializing home directory with user template for $lang"
-	ditto /System/Library/User\ Template/Non_localized $username
-	ditto /System/Library/User\ Template/$lang.lproj $username
-	sudo chown -R $username:$groupname $username
+	echo "Initializing home directory with user template for $lang" | tee -a /var/log/physreg-home.log
+	ditto /System/Library/User\ Template/Non_localized $username 2>&1 | tee -a /var/log/physreg-home.log
+	ditto /System/Library/User\ Template/$lang.lproj $username 2>&1 | tee -a /var/log/physreg-home.log
+	chown -R $username:$groupname $username 2>&1 | tee -a /var/log/physreg-home.log
+else
+	echo "Homedir already initialized" | tee -a /var/log/physreg-home.log
 fi
 
 exit 0
