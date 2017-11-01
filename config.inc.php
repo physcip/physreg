d3ca3d7f97db8dbb15b4e26efbc38f8a25dc5133
Jeija <norrepli@gmail.com>
2017-11-01 11:53:46 +0100

    Move verify.php to seperate physunreg repository
    Disabling user accounts is now handled and documented in physunreg which
    includes physreg as a git submodule and uses physreg configuration options
    from config.inc.php and config_secret.inc.php

--- config.inc.php
+++ config.inc.php
@@ -31,23 +31,21 @@ $PHYSCIP_PRIMARYGROUP = "cn=cipuser,ou=Groups,dc=physcip,dc=uni-stuttgart,dc=de"
 $PHYSCIP_PRIMARYGROUPID = 1104;										# Last block of SID (= RID), for Windows primary group
 $PHYSCIP_GIDNUMBER = 10000;										# gidNumber of primary group
 
-# Groups that are allowed to register new accounts ($ALLOWEDGROUPS) and groups whose member's accounts
-# won't be deleted ($KEEPGROUPS), but whose members can't register new accounts. $ALLOWEDUSERS is a list
-# of users that can create new accounts despite not being a member of $ALLOWEDGROUPS.
+# $ALLOWEDGROUPS: Groups whose members are allowed to register new accounts
 $ALLOWEDGROUPS=array(
 	"CN=Stg1590-08-128,OU=OrgGroups,OU=IDMGroups,OU=SIAM,DC=stud,DC=uni-stuttgart,DC=de",
 	"CN=Stg1590-08-918,OU=OrgGroups,OU=IDMGroups,OU=SIAM,DC=stud,DC=uni-stuttgart,DC=de",
 );
-$KEEPGROUPS=array(
-	// Doktoranden sind bei der Fakultät geführt. Mathe-Doktoranden sollen sich aber nicht registrieren können.
-	// Deswegen müssen neue Physik-Doktoranden manuell freigeschaltet werden, werden aber nicht automatisch deaktiviert.
-	"CN=Stg1590-08-P82,OU=OrgGroups,OU=IDMGroups,OU=SIAM,DC=stud,DC=uni-stuttgart,DC=de",
-);
-$allowfile = "/etc/phyreg-allow";
-if (file_exists($allowfile))
-	$ALLOWEDUSERS=file($allowfile, FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+
+# $ALLOWEDUSERS: List of users that have a special permission to create new accounts
+# (despite not being a member of a group in $ALLOWEDGROUPS), e.g. students minoring in physics
+# These special permissions are usually only assigned for the duration of a single semester.
+$ALLOWFILE = "/etc/phyreg-allow";
+$ALLOWEDUSERS = [];
+if (file_exists($ALLOWFILE))
+	$ALLOWEDUSERS=file($ALLOWFILE, FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
 else
-	trigger_error("$allowfile missing.", E_USER_WARNING);
+	trigger_error("$ALLOWFILE missing.", E_USER_WARNING);
 
 $allowed_v4 = array('129.69.0.0/16', '141.58.0.0/16', '192.108.35.0/24', '192.108.36.0/22', '192.108.40.0/22', '192.108.44.0/24');
 $allowed_v6 = array('fe80::/64', '2001:7C0:7C0::/48', '2001:7C0:2000::/40', '2001:638:202::/48', 'FC5C:983E:D7E3::/48');
